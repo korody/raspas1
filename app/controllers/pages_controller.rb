72b8bd13ab679cb281717e4a0609c38b1138bf09
korody <marcoskorody@hotmail.com>
2012-03-29 01:10:52 -0300

    layout adaptado

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
       @user = User.new
       @authors = Author.all
       @users = User.all
-      @tags = Tag.all
+      @tags = Tag.all(:order => 'tags.created_at DESC')
       @feed_items = Micropost.all(:order => 'microposts.created_at DESC', :limit => 20)
     end
   end
