043612d659ab6e3d05ea0829bab63534450541ba
Marcos Korody <marcoskorody@hotmail.com>
2012-04-19 01:17:04 -0300

    tests

--- db/migrate/20120418020929_remove_published_to_authors.rb
+++
@@ -1,9 +0,0 @@
-class RemovePublishedToAuthors < ActiveRecord::Migration
-  def up
-    remove_column :authors, :published
-  end
-
-  def down
-    add_column :authors, :published, :boolean
-  end
-end
