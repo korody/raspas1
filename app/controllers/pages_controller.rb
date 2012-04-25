97b30071b2329ee2697a981ea3fb0d46f905e38b
Marcos Korody <marcoskorody@hotmail.com>
2012-04-25 12:05:01 -0300

    Facebook Tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@
     @user = User.new
     @feed_items = Micropost.all(:order => 'microposts.created_at DESC', :limit => 20)
     @micropost = Micropost.new
+    @microposts = Micropost.find(params[:id])
     @authors = Author.all(:order => 'authors.created_at DESC')
     @users = User.all(:order => 'users.created_at DESC')
     @tags = Tag.all(:order => 'tags.created_at DESC')
