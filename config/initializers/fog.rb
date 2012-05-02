68207b19639c8de7ee6fcd04ae73564c7233fcc7
Marcos Korody <marcoskorody@hotmail.com>
2012-05-02 12:39:01 -0300

    amazon tests

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,13 +1,22 @@
-CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp')
-  config.cache_dir = 'uploads'
+if Rails.env.test? # Store the files locally for test environment
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+end
 
-  config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAJYBTFMGBKR2YOIDQ',       # required
-    :aws_secret_access_key  => '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',       # required
-    :region                 => 'sa-east-1'  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'raspas.images'                     # required
-  config.fog_host       = false                                  # optional, defaults to true
+if Rails.env.development? or Rails.env.production? # Using Amazon S3 for Development and Production
+  CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'uploads'
+
+    config.fog_credentials = {
+      :provider               => 'AWS',       # required
+      :aws_access_key_id      => 'AKIAJYBTFMGBKR2YOIDQ',       # required
+      :aws_secret_access_key  => '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',       # required
+      :region                 => 'sa-east-1'  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = 'raspas.images'                     # required
+    config.fog_host       = false                                  # optional, defaults to true
+  end
 end
