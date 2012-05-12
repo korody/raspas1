0d692f642becff68e2cd3c5ff5f3b44a9fa86be7
Marcos Korody <marcoskorody@hotmail.com>
2012-05-12 03:17:45 -0300

    layout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,11 @@
 # encoding: utf-8
 module SessionsHelper
 
-	def current_user
+  def home
+    request.fullpath == root_path
+  end	
+
+  def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
