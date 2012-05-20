97e51b2151be605066a13d8a2c641ac548132669
Marcos Korody <marcoskorody@hotmail.com>
2012-05-20 14:56:32 -0300

    uniq

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,9 +55,9 @@
 
   has_many :idols, through: :subscriptions, source: :author
 
-  has_many :authors, through: :microposts, order: "authors.created_at DESC", uniq: true#, select: "DISTINCT authors.*"
+  has_many :authors, through: :microposts, order: "authors.created_at DESC", select: "DISTINCT authors.*"
 
-  has_many :tags, through: :microposts, order: "tags.created_at DESC", uniq: true#, select: "DISTINCT tags.*"
+  has_many :tags, through: :microposts, order: "tags.created_at DESC", select: "DISTINCT tags.*"
 
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
