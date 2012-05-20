91a4348045289bb4241b90ca8377eb0ebc04d980
Marcos Korody <marcoskorody@hotmail.com>
2012-05-20 15:11:13 -0300

    uniq

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@
                                    :class_name => "Favourite",
                                    :dependent => :destroy
 
-  has_many :favoritadas, through: :reverse_favourites, source: :micropost, order: "favourites.created_at DESC"#, select: "DISTINCT favourites.*"
+  has_many :favoritadas, through: :reverse_favourites, source: :micropost, order: "favourites.created_at DESC", select: "DISTINCT favourites.*"
 
   has_many :relationships, :foreign_key => "follower_id",
                            :dependent => :destroy
