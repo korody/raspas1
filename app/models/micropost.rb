40748edec09d74665fce3e53ae66473c8e49dcf8
Marcos Korody <marcoskorody@hotmail.com>
2012-05-20 15:45:00 -0300

    uniq

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -38,9 +38,6 @@ class Micropost < ActiveRecord::Base
   scope :from_microposts_favourites_of, lambda { |micropost| favourites_of(micropost) }
   scope :user_feed, lambda { |user| from_users_followed_by(user).concat(from_authors_idols_of(user)).concat(from_microposts_favourites_of(user)) }
 
-  scope :from_microposts_favourites_of, order: "microposts.created_at DESC"
-  scope :user_feed, order: "microposts.created_at DESC"
-
   attr_accessor :author_name
   attr_writer :tag_names
   attr_reader :tag_tokens
