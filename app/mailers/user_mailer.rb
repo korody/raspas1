6dece11f1459ce43920275005c4274079cbe8de2
Marcos Korody <marcoskorody@hotmail.com>
2012-06-02 00:20:46 -0300

    action mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-	default :from => "admin@raspas.com.br"
-  
-	def registration_confirmation(user)
-		#@user = user
-		#attachments["rails.png"] = File.read("#{Rails.root}/public/images/rails.png")
-		mail(to: "user.email", subject: "Bem-vindo ao Raspas!")
-	end
-end
--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => "raspas.com.br",
-      :user_name            => "admin@raspas.com.br",
-      :password             => "nddssssef",
-      :authentication       => :plain,
-      :enable_starttls_auto => true
-    }
