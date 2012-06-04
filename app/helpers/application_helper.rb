74d3df104f8c35c870557252b17731836f2553b6
Marcos Korody <marcoskorody@hotmail.com>
2012-06-03 22:25:49 -0300

    sharer test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,15 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
 	def title
-    	base_title = "raspas"
-    	if @title.nil?
-      	base_title
-      else      
-        "#{base_title} | #{@title}"
+      if !@micropost
+      	base_title = "raspas"
+      	if @title.nil?
+        	base_title
+        else      
+          "#{base_title} | #{@title}"
+        end
+      else
+        @micropost.content  
       end
   end
 
