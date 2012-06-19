f242c31df5c7ea93bfb1e9ce81d786cb1734bac6
Marcos Korody <marcoskorody@hotmail.com>
2012-06-19 17:15:54 -0300

    admin

--- app/admin/taggings.rb
+++ app/admin/taggings.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Tagging do
 	index do
 		column :micropost do |micropost|
-	        link_to micropost.id, [:admin, tagging]
+	        link_to micropost.id, [:admin, micropost]
 	      end
 		column :tag
 		default_actions
