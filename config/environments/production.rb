9459fa983d56ef6c17c74198dea0990c39b2f0af
Marcos Korody <marcoskorody@hotmail.com>
2012-07-30 23:45:44 -0300

    add improvements

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Benfeitor::Application.configure do
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
 
-  Use a different cache store in production
-  config.cache_store = :mem_cache_store
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
