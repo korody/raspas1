166a28f7e91fce5832883426cb8b81c4a72ba6b4
Marcos Korody <marcoskorody@hotmail.com>
2012-07-31 02:27:16 -0300

    pg search tests

--- app/models/author.rb
+++ app/models/author.rb
@@ -46,9 +46,9 @@ class Author < ActiveRecord::Base
 
   def self.text_search(query)
     if query.present?
-      where("name @@ :q", q: query)
+      search(query)
     else
-      find(:all)
+      scoped
     end
   end
 
