d204b11ff00c6e6f80413e25798e60509a864249
Marcos Korody <marcoskorody@hotmail.com>
2012-08-01 02:54:24 -0300

    testes

--- app/models/author.rb
+++ app/models/author.rb
@@ -40,8 +40,8 @@ class Author < ActiveRecord::Base
 
   include PgSearch
   pg_search_scope :search, against: [:name, :job],
-    using: {tsearch: {:prefix => true}, {dictionary: "portuguese"}},
-    associated_against: {users: :name},
+    using: {tsearch: {prefix: true, dictionary: "portuguese"}},
+    associated_against: {users: :name]},
     ignoring: :accents  
 
   def self.text_search(query)
