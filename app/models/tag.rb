ce8d829e098b83440d50b84337032b363b4d43e5
Marcos Korody <marcoskorody@hotmail.com>
2012-08-07 03:13:59 -0300

    add ajax to favourites button

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -24,7 +24,6 @@ class Tag < ActiveRecord::Base
  include PgSearch
   pg_search_scope :search, against: :name,
     using: {tsearch: {prefix: true, dictionary: "portuguese"}},
-    associated_against: {authors: :name},
     ignoring: :accents  
 
   def self.text_search(query)
