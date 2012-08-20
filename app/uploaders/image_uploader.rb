50f4c5634c37f87ecf0d9052d1b83c80ba1b7527
Marcos Korody <marcoskorody@hotmail.com>
2012-08-20 11:16:03 -0300

    add origins model

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -46,11 +46,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :regular do
-    process :resize_to_fill => [200, 200]
+    process :resize_to_fill => [200, 600]
   end
 
-  version :profile do
-    process :resize_to_fit => [200, 600]
+  version :card do
+    process :resize_to_fit => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
