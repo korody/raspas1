f6b5f01e7a15d7529fbdf3976ddea9fdc9198687
Marcos Korody <marcoskorody@hotmail.com>
2012-08-23 00:54:59 -0300

    add user_id to authors, bio_poster, auto_link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    raise request.env["omniauth.auth"].to_yaml
+    auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
     email = auth['info']['email']
     if user
