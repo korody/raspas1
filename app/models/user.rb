89618a15da121cd8c35b9ab4caee41f811e0d9c8
Marcos Korody <marcoskorody@hotmail.com>
2012-08-23 01:27:07 -0300

    add user_id to authors, bio_poster, auto_link

--- app/models/user.rb
+++ app/models/user.rb
@@ -94,6 +94,7 @@
       user.origin = auth["info"]["location"]
       user.bio = auth["info"]["description"]
       user.image = auth["info"]["image"]
+      user.tipo = auth["info"]["Twitter"]
       #user.image = auth["extra"]["raw_info"]["image"] 
     end
   end
