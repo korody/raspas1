2a2db064299282f1ceda3602d8abe5855c4159e7
Korody <marcoskorody@hotmail.com>
2012-10-25 17:04:19 -0200

    fog fixes

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -17,6 +17,6 @@ if Rails.env.development? or Rails.env.production? # Using Amazon S3 for Develop
       :region                 => 'sa-east-1'  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = 'raspasimages'                     # required
-    config.asset_host     = 'https://assets.example.com'            # optional, defaults to nil
+    # config.fog_host       = false                                  # optional, defaults to true
   end
 end
