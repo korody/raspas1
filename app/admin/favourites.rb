1c2ad1bc4e3b2c205e46b556c5ae98dbea65e968
korody <marcoskorody@hotmail.com>
2012-11-22 20:18:19 -0200

    Author Search Against -microposts content, admin fix

--- app/admin/favourites.rb
+++ app/admin/favourites.rb
@@ -6,5 +6,16 @@ ActiveAdmin.register Favourite do
 		column :author_id
 		column :poster_id
 		default_actions
-	end  
+	end
+
+	form do |f|
+	    f.inputs "Favourites" do
+	    	column :id
+	    	column :user_id
+	    	column :micropost_id
+	    	column :poster_id
+	    end
+	    f.buttons
+  	end  
 end
+
--- app/admin/taggings.rb
+++ app/admin/taggings.rb
@@ -1,15 +1,15 @@
 ActiveAdmin.register Tagging do
 	index do
 		column :micropost do |micropost|
-	        link_to micropost.id, [:admin, micropost]
-	      end
+	    	link_to micropost.id, [:admin, micropost]
+	    end
 		column :tag
 		default_actions
 	end
 
 	form do |f|
 	    f.inputs "Taggings" do
-			f.inputs :micropost
+			f.inputs :micropost_id
 			f.inputs :tag
 	    end
 	    f.buttons
