ffce547460cb56ea59fc3d36c5445c3cc7a20bb9
korody <marcoskorody@hotmail.com>
2013-06-10 01:32:56 -0300

    jquery error persisted

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,39 +2,39 @@
 class TagsController < ApplicationController
 	
 	def index
-	    @title = "Temas de Pensamentos e Frases"
-  		@new_micropost = Micropost.new
-	    @tags = Tag.all(order: :name)
-  	end
+    @title = "Temas de Pensamentos e Frases"
+		@new_micropost = Micropost.new
+    @tags = Tag.all(order: :name)
+	end
 
-  	def show
-	    @tag = Tag.find(params[:id])
-	    @title = "Pensamentos e Frases sobre #{@tag.name}"
-	    @tags = Tag.all(order: 'tags.created_at DESC')
-	    @users = @tag.users(order: 'tags.created_at DESC')
-	    @authors = @tag.authors(order: 'tags.created_at DESC')
-	    @microposts = @tag.microposts.paginate(page: params[:page])
-	    @new_micropost = Micropost.new
+	def show
+    @tag = Tag.find(params[:id])
+    @title = "Pensamentos e Frases sobre #{@tag.name}"
+    @tags = Tag.all(order: 'tags.created_at DESC')
+    @users = @tag.users(order: 'tags.created_at DESC')
+    @authors = @tag.authors(order: 'tags.created_at DESC')
+    @microposts = @tag.microposts.paginate(page: params[:page])
+    @new_micropost = Micropost.new
 	end
 
 	def autocomplete
-	    @tags = Tag.order(:name).search("%#{params[:term]}%")
-   		render json: @tags.map(&:name) 
-   	end
+    @tags = Tag.order(:name).search("%#{params[:term]}%")
+ 		render json: @tags.map(&:name) 
+ 	end
 
-   	def users
-      @tag = Tag.find(params[:id])
-	    @title = "pelos usuários sobre #{@tag.name}"
-      @users = @tag.users.paginate(page: params[:page])
-      @new_micropost = Micropost.new
-      render 'show_users'
-  	end
+ 	def users
+    @tag = Tag.find(params[:id])
+    @title = "pelos usuários sobre #{@tag.name}"
+    @users = @tag.users.paginate(page: params[:page])
+    @new_micropost = Micropost.new
+    render 'show_users'
+	end
 
-  	def authors
-      @tag = Tag.find(params[:id])
-	    @title = "dos pensadores sobre #{@tag.name}"
-      @authors = @tag.authors.paginate(page: params[:page])
-      @new_micropost = Micropost.new
-      render 'show_authors'
-  	end		
+	def authors
+    @tag = Tag.find(params[:id])
+    @title = "dos pensadores sobre #{@tag.name}"
+    @authors = @tag.authors.paginate(page: params[:page])
+    @new_micropost = Micropost.new
+    render 'show_authors'
+	end		
 end
