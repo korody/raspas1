4986b67d27db5aa71fc88b4155eff0e78be0ecd1
korody <marcoskorody@hotmail.com>
2014-05-30 00:13:52 -0300

    assets testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,6 @@ Benfeitor::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  config.eager_load = true
-
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -14,10 +12,10 @@ Benfeitor::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.js_compressor  = :uglifier
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
