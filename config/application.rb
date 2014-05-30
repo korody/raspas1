993c76a4905c010111ff0be3e5e1c7b82126b677
korody <marcoskorody@hotmail.com>
2014-05-30 00:11:31 -0300

    cache testing

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module Benfeitor
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
