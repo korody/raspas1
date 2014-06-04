51c48103983fa0eb61b9a075acb0553a1c1f7dd8
korody <marcoskorody@hotmail.com>
2014-06-03 22:25:46 -0300

    force http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_filter do
     if request.ssl? || Rails.env.production? 
-      redirect_to protocol: 'http://'
+      redirect_to :protocol => 'http://', status => :moved_permanently
     end
   end	
 end
