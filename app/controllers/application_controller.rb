023b039707fe69b2d2f7ab2382851005f1c0baa1
korody <marcoskorody@hotmail.com>
2014-06-03 22:42:24 -0300

    abort force http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include SessionsHelper
-
-  before_filter do
-    if request.ssl? || Rails.env.production? 
-      redirect_to :protocol => 'http://'
-    end
-  end	
+  include SessionsHelper	
 end
